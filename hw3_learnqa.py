2327489d328457b7021b2c02f0ec3fa8458d3533
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2021-06-25 20:25:54 +0300

    Update hw3_learnqa.py

--- hw3_learnqa.py
+++ hw3_learnqa.py
@@ -2,8 +2,8 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True) #разрешаем редиректы
 
-more_redir = response.history #узнаем кол-во редиректов
-print(more_redir)
+all_redir = len(response.history) #узнаем кол-во редиректов
+print(str(all_redir) + ' Кол-во' + ' редиректов')
 
 first_redir = response.history[0] #редирект первый
 second_redir = response.history[1] #редирект второй
