46e8c43a7281a02e7f1ffc37f725880e9685fa9e
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2021-06-27 15:09:01 +0300

    Update learnqahw4_123punkti.py

--- learnqahw4_123punkti.py
+++ learnqahw4_123punkti.py
@@ -3,8 +3,8 @@ import requests
 response1 = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
 print(response1.text) #Wrong method provided пункт первый
 
-response3 = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type?method=HEAD")
+response3 = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method":"HEAD"})
 print(response3.text) #пустой вывод пункт второй
 
-response4 = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type?method=PUT")
+response4 = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method":"PUT"})
 print(response4.text) #{"success":"!"} третий пункт
