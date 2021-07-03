22d2c8770b9633a566a634cd943b75c232d05ba4
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2021-07-03 13:12:13 +0300

    Update module3_2.py

--- module3_2.py
+++ module3_2.py
@@ -1,8 +1,11 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/homework_cookie")
-pars_cookie = dict(response.cookies)
-print(pars_cookie)
-check_actual_cookie = {'HomeWork': 'hw_value'}
+class TestCookie():
+    def test_check_cookie(self):
 
-assert pars_cookie == check_actual_cookie
+        response = requests.post("https://playground.learnqa.ru/api/homework_cookie")
+        pars_cookie = dict(response.cookies)
+        print(pars_cookie)
+        check_actual_cookie = {'HomeWork': 'hw_value'}
+
+        assert pars_cookie == check_actual_cookie
